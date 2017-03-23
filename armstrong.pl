4566a14ec8a15cf62de6a9a1538840f5887627f3
veenadevasia <veenadevasia@gmail.com>
2017-03-23 11:42:45 +0530

    checking armstrong number

---
+++ armstrong.pl
@@ -0,0 +1,21 @@
+print "Enter the number: \n";
+$n=<>;
+$t=$n;
+$s=0;
+while($n>0)
+{
+$r=$n%10;
+$i=$r*$r*$r;
+$s=$s+$i;
+$n=$n/10;
+}
+
+if($s==$t)
+{
+print "Armstrong number\n";
+}
+else
+{
+print "Not armstrong number\n";
+}
+
