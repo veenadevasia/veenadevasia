c8d7b6973fab07a86ec2deaba5a54d00d3803729
veenadevasia <veenadevasia@gmail.com>
2017-03-23 11:38:47 +0530

    frequency of terms

---
+++ frequency.pl
@@ -0,0 +1,13 @@
+$file=$ARGV[0];
+$word=$ARGV[1];
+open (my $fh,'<:encoding(UTF-8)', $file) or die "could not open filename '$file' $!";
+$total=0;
+while(my $line = <$fh>)
+{
+my @match = $line = -m/($word)/g;
+my $count = scalar (@match);
+$total = $total+$count;
+#print "$count\t$line\n";
+}
+print "word frequency: $total\n";
+
